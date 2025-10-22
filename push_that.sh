870958265d6d082d331f268e3383dfb99bc112db
Nathan Amaraggi <nathan.amaraggi@epitech.eu>
2025-10-22 13:33:11 +0200

    init

--- push_that.sh
+++ push_that.sh
@@ -3,5 +3,5 @@ read -p "Commit name : " COM_NAME
 git add -A
 git commit -m "$COM_NAME"
 git branch -M main
-git remote add origin SSH //SSH to be replaced with ssh key to your repository
+git remote add origin git@github.com:Jarjarbin06/Epitech_Base.git
 git push -u origin main
