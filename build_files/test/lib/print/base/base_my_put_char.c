811e5c4e8527f538584bf75ef4da6dc05cffbac4
JARJARBIN's studio <105795201+Jarjarbin06@users.noreply.github.com>
2026-03-30 14:16:10 +0200

    [INIT] Repo initialization or library import

---
+++ build_files/test/lib/print/base/base_my_put_char.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** my_printf.c
+** File description:
+** None
+*/
+
+#include <unistd.h>
+#include "print.h"
+
+void p_put_char(const char c, int *char_total)
+{
+    write(1, &c, 1);
+    *char_total += 1;
+}
